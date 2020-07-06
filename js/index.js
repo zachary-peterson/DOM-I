cbfe106e4ff04982eb410aa3f2193f92f54c4bf1
Zachary Peterson <zp.lambda@yahoo.com>
2020-07-06 15:34:26 -0400

    Progress

--- js/index.js
+++ js/index.js
@@ -40,3 +40,30 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let logoImage = document.querySelector('#logo-img');
+logoImage.src = "img/logo.png"
+
+let ctaImage = document.querySelector('#cta-img');
+ctaImage.src = "img/header-img.png"
+
+let midImage = document.querySelector('#middle-img');
+midImage.src = "img/mid-page-accent.jpg"
+
+let navItems = document.querySelectorAll('a');
+console.log(navItems);
+
+navItems[0].textContent = siteContent.nav['nav-item-1'];
+navItems[1].textContent = siteContent.nav['nav-item-2'];
+navItems[2].textContent = siteContent.nav['nav-item-3'];
+navItems[3].textContent = siteContent.nav['nav-item-4'];
+navItems[4].textContent = siteContent.nav['nav-item-5'];
+navItems[5].textContent = siteContent.nav['nav-item-6'];
+
+let ctaTextOne = document.querySelector('.cta-text h1');
+ctaTextOne.textContent = siteContent.cta['h1'];
+
+let ctaTextTwo = document.querySelector('.cta-text button');
+ctaTextTwo.textContent =siteContent.cta['button'];
+
+let mainOne = document.querySelector('')
